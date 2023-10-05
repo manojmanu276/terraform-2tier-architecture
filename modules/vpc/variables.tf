0d7f659fd717ef4acfa0b6c9bbd98efa9b427c7d
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-10-05 17:42:54 +0530

    declared variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable region {}
+variable project_name {}
+variable vpc_cidr {}
+variable pub_sub_1a_cidr {}
+variable pub_sub_2b_cidr {}
+variable prv_sub_3a_cidr {}
+variable prv_sub_4b_cidr {}
+variable prv_sub_5a_cidr {}
+variable prv_sub_6b_cidr {}
