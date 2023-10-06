a6c485d50bc5a41d176c921872e39be19208dff6
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-10-06 14:39:15 +0530

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,11 @@
+variable "db_sub_name" {}
+variable "db_username" {}
+variable "db_password" {}
+variable "prv_sub_5a_id" {}
+variable "prv_sub_6b_id" {}
+variable "db_name" {
+  default = "test-db"
+}
+variable "db_sg_id" {
+  default = "mydb-subnet-a-b"
+}
