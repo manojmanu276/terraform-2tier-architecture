b4de8479a6c76e16ca5e42998954d47d5812c64f
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-10-06 14:12:48 +0530

    Create main.tf

---
+++ modules/route53/main.tf
@@ -0,0 +1,16 @@
+data "aws_route53_zone" "public_zone" {
+  name         = var.hosted_zone_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "cloudfront_record" {
+  zone_id = data.aws_route53_zone.public_zone.zone_id
+  name    = "manoj.${data.aws_route53_zone.public_zone.name}"
+  type    = "A"
+
+  alias {
+    name                   = var.cloudfront_domain_name
+    zone_id                = var.cloudfront_hosted_zone_id
+    evaluate_target_health = false
+  }
+}
